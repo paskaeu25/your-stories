b540754f6626c884f9b2eb5afddf7e29a22beeda
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-01-09 13:04:55 +0200

    finished adding story functionality.

---
+++ routes/stories.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const Story = require('../models/Story');
+const { ensureAuth } = require('../middleware/auth');
+
+// @desc    Show add page
+// @route   GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+  res.render('stories/add');
+});
+
+// @desc    Process add form
+// @route   POST /stories
+router.post('/', ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    console.log(req.body);
+    await Story.create(req.body);
+    res.redirect('/dashboard');
+  } catch (error) {
+    console.log(error);
+    res.render('error/500');
+  }
+});
+
+// @desc    Show all stories
+// @route   GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: 'public' })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean();
+
+    res.render('stories/index', { stories });
+  } catch (error) {
+    console.log(error);
+    res.render('error/500');
+  }
+});
+
+module.exports = router;
