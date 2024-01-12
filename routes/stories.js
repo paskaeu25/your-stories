353922e96e261c23c7979bb052fef266c4b2f089
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-01-12 13:45:58 +0200

    done single story page

--- routes/stories.js
+++ routes/stories.js
@@ -39,6 +39,24 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 });
 
+// @desc    Show a single story
+// @route   GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate('user').lean();
+
+    if (!story) {
+      return res.render('error/404');
+    }
+
+    res.render('stories/show', {
+      story,
+    });
+  } catch (error) {
+    return res.render('error/404');
+  }
+});
+
 // @desc    Show edit page
 // @route   GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
